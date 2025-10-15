41814ce77370ec76cb1ebf3b6ae6d1733b2da933
RADONWORK <darkcomputer46@gmail.com>
2025-10-15 13:11:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
 import './App.css';
 import Events from './components/Events';
-
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
-
+      <Events />
     </div>
   );
 }
